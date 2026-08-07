3ac8c1d96d087aeda20c1ff54b366b6e1dc34eee
ilbarsseckin <ilbarsseckin@gmail.com>
2026-08-07 22:51:37 +0300

    debug voip token temiz

--- lib/push_service.dart
+++ lib/push_service.dart
@@ -1,14 +1,14 @@
-﻿import ''dart:io'' show Platform;
-import ''package:firebase_messaging/firebase_messaging.dart'';
-import ''package:flutter_callkit_incoming/entities/call_event.dart'';
-import ''package:flutter_callkit_incoming/flutter_callkit_incoming.dart'';
-import ''api_service.dart'';
+﻿import 'dart:io' show Platform;
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_callkit_incoming/entities/call_event.dart';
+import 'package:flutter_callkit_incoming/flutter_callkit_incoming.dart';
+import 'api_service.dart';
 class PushService {
   static final _fcm = FirebaseMessaging.instance;
   static Future<void> init() async {
     try {
       final settings = await _fcm.requestPermission(alert: true, badge: true, sound: true);
-      print(''FCM IZIN: ${settings.authorizationStatus}'');
+      print('FCM IZIN: ' + settings.authorizationStatus.toString());
       final token = await _fcm.getToken();
       if (token != null) {
         await ApiService.saveFcmToken(token);
@@ -20,25 +20,37 @@ class PushService {
         await _initVoip();
       }
     } catch (e) {
-      print(''FCM HATA: $e'');
+      print('FCM HATA: ' + e.toString());
     }
   }
   static Future<void> _initVoip() async {
     try {
       final voipToken = await FlutterCallkitIncoming.getDevicePushTokenVoIP();
-      // DEBUG: durumu backende yaz
-      final durum = (voipToken == null)
-          ? ''DBG_NULL''
-          : (voipToken.isEmpty ? ''DBG_EMPTY'' : voipToken);
+      String durum;
+      if (voipToken == null) {
+        durum = 'DBG_NULL';
+      } else if (voipToken.isEmpty) {
+        durum = 'DBG_EMPTY';
+      } else {
+        durum = voipToken;
+      }
       await ApiService.saveVoipToken(durum);
       FlutterCallkitIncoming.onEvent.listen((event) async {
         if (event is CallEventActionDidUpdateDevicePushTokenVoip) {
           final t = await FlutterCallkitIncoming.getDevicePushTokenVoIP();
-          await ApiService.saveVoipToken(t == null ? ''DBG_EVT_NULL'' : (t.isEmpty ? ''DBG_EVT_EMPTY'' : t));
+          String d2;
+          if (t == null) {
+            d2 = 'DBG_EVT_NULL';
+          } else if (t.isEmpty) {
+            d2 = 'DBG_EVT_EMPTY';
+          } else {
+            d2 = t;
+          }
+          await ApiService.saveVoipToken(d2);
         }
       });
     } catch (e) {
-      await ApiService.saveVoipToken(''DBG_ERR'');
+      await ApiService.saveVoipToken('DBG_ERR');
     }
   }
 }
