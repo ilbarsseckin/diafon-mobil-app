db8180bf82de26ae0b89583fd11645e6dbaec1dc
ilbarsseckin <ilbarsseckin@gmail.com>
2026-08-08 23:05:17 +0300

    debug prefs anahtar tespiti

--- lib/push_service.dart
+++ lib/push_service.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 import 'api_service.dart';
 class PushService {
   static final _fcm = FirebaseMessaging.instance;
@@ -18,26 +19,37 @@ class PushService {
       if (Platform.isIOS) {
         _initVoip();
       }
-    } catch (e) {
-      print('FCM HATA: ' + e.toString());
-    }
+    } catch (e) {}
+  }
+  static Future<void> _dbg(String msg) async {
+    try {
+      await http.post(
+        Uri.parse('https://mobildiafon.com/api/auth/voip-debug'),
+        headers: {'Content-Type': 'application/json'},
+        body: '{"dbg":"' + msg + '"}',
+      );
+    } catch (e) {}
   }
   static Future<void> _initVoip() async {
-    // AppDelegate token'i UserDefaults'a yaziyor. shared_preferences ile oku.
     for (int i = 0; i < 20; i++) {
       await Future.delayed(const Duration(seconds: 2));
       try {
         final prefs = await SharedPreferences.getInstance();
         await prefs.reload();
+        final keys = prefs.getKeys().toList();
         final voipToken = prefs.getString('voip_device_token');
+        if (i == 3) {
+          await _dbg('anahtarlar=' + keys.join(','));
+        }
         if (voipToken != null && voipToken.isNotEmpty) {
+          await _dbg('TOKEN_BULUNDU_len=' + voipToken.length.toString());
           await ApiService.saveVoipToken(voipToken);
-          print('VOIP TOKEN gonderildi (prefs)');
           return;
         }
       } catch (e) {
-        print('VOIP prefs hata: ' + e.toString());
+        await _dbg('HATA=' + e.toString());
       }
     }
+    await _dbg('TOKEN_BULUNAMADI_20_deneme');
   }
 }
