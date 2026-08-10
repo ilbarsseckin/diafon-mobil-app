495b59ed04aefebfbfc888ac61a083275cc5de1c
ilbarsseckin <ilbarsseckin@gmail.com>
2026-08-10 10:38:12 +0300

    protokol 7 metod tam providerDidReset dahil

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -56,24 +56,28 @@ import flutter_callkit_incoming
     completion()
   }
 
-  func onAccept(_ call: flutter_callkit_incoming.Call, _ action: CXAnswerCallAction) {
+  // MARK: - CallkitIncomingAppDelegate (7 metod)
+  func onAccept(_ call: Call, _ action: CXAnswerCallAction) {
     action.fulfill()
   }
 
-  func onDecline(_ call: flutter_callkit_incoming.Call, _ action: CXEndCallAction) {
+  func onDecline(_ call: Call, _ action: CXEndCallAction) {
     action.fulfill()
   }
 
-  func onEnd(_ call: flutter_callkit_incoming.Call, _ action: CXEndCallAction) {
+  func onEnd(_ call: Call, _ action: CXEndCallAction) {
     action.fulfill()
   }
 
-  func onTimeOut(_ call: flutter_callkit_incoming.Call) {
+  func onTimeOut(_ call: Call) {
   }
 
   func didActivateAudioSession(_ audioSession: AVAudioSession) {
   }
 
   func didDeactivateAudioSession(_ audioSession: AVAudioSession) {
+  }
+
+  func providerDidReset() {
   }
 }
